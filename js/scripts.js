dd60916688f4244259b96c1940f8ba7db0f4a3d0
Ryan Tucker <nick.tucker.bass@gmail.com>
2021-06-23 18:15:41 -0400

    adding JS animations

--- js/scripts.js
+++ js/scripts.js
@@ -63,3 +63,88 @@ window.addEventListener('scroll', () => {
   let offset = window.pageYOffset
   parallax.style.backgroundPositionY = offset*.6+'px'
 })
+
+//
+
+/* SLIDE UP */
+let slideUp = (target, duration=500) => {
+
+  target.style.transitionProperty = 'height, margin, padding';
+  target.style.transitionDuration = duration + 'ms';
+  target.style.boxSizing = 'border-box';
+  target.style.height = target.offsetHeight + 'px';
+  target.offsetHeight;
+  target.style.overflow = 'hidden';
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  window.setTimeout( () => {
+        target.style.display = 'none';
+        target.style.removeProperty('height');
+        target.style.removeProperty('padding-top');
+        target.style.removeProperty('padding-bottom');
+        target.style.removeProperty('margin-top');
+        target.style.removeProperty('margin-bottom');
+        target.style.removeProperty('overflow');
+        target.style.removeProperty('transition-duration');
+        target.style.removeProperty('transition-property');
+        //alert("!");
+  }, duration);
+}
+
+// *** *** from: https://dev.to/bmsvieira/vanilla-js-slidedown-up-4dkn *** *** //
+
+/* SLIDE DOWN */
+let slideDown = (target, duration=500) => {
+
+  target.style.removeProperty('display');
+  let display = window.getComputedStyle(target).display;
+  if (display === 'none') display = 'block';
+  target.style.display = display;
+  let height = target.offsetHeight;
+  target.style.overflow = 'hidden';
+  target.style.height = 0;
+  target.style.paddingTop = 0;
+  target.style.paddingBottom = 0;
+  target.style.marginTop = 0;
+  target.style.marginBottom = 0;
+  target.offsetHeight;
+  target.style.boxSizing = 'border-box';
+  target.style.transitionProperty = "height, margin, padding";
+  target.style.transitionDuration = duration + 'ms';
+  target.style.height = height + 'px';
+  target.style.removeProperty('padding-top');
+  target.style.removeProperty('padding-bottom');
+  target.style.removeProperty('margin-top');
+  target.style.removeProperty('margin-bottom');
+  window.setTimeout( () => {
+    target.style.removeProperty('height');
+    target.style.removeProperty('overflow');
+    target.style.removeProperty('transition-duration');
+    target.style.removeProperty('transition-property');
+  }, duration);
+}
+
+/* TOOGLE */
+var slideToggle = (target, duration = 500) => {
+  if (window.getComputedStyle(target).display === 'none') {
+    return slideDown(target, duration);
+  } else {
+    return slideUp(target, duration);
+  }
+}
+
+let introSlide = document.querySelectorAll('.slide')
+let aboutSlide = document.querySelectorAll('.about-slide')
+
+for(let div of introSlide){
+  slideDown(div, 1000)
+}
+
+for(let div of aboutSlide){
+  slideDown(div, 500)
+}
+
+
